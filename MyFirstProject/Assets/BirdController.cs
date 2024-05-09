06935da7d47c0f53f7d2fddae69bb586ed48d85b
Jan Buliński <jbulinski@edu.cdv.pl>
2024-05-09 13:01:09 +0200

    added flappy bird

---
+++ MyFirstProject/Assets/BirdController.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BirdController : MonoBehaviour
+{
+    public Rigidbody2D rb2D;
+    public float JumpForce;
+    void Start()
+    {
+        Debug.Log("Hello World!");
+    }
+
+    void Update()
+    {
+        if(Input.GetButtonDown("Jump"))
+        {
+            rb2D.AddForce(new Vector2(0, JumpForce), ForceMode2D.Impulse);
+        }
+    }
+}
